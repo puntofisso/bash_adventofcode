26c191496c18850ffba4695ee5a22130ffcd64d2
Giuseppe Sollazzo <puntofisso@gmail.com>
2015-12-06 21:20:53 +0000

    Testing day6

--- day6-1.sh
+++ day6-1.sh
@@ -1,20 +1,64 @@
 #!/bin/bash
 declare -A matrix
-rows=1000
-cols=1000
+rows=999
+cols=999
 
 #all lights are off
-for ((i=1;i<=rows;i++)) do
-    for ((j=1;j<=columns;j++)) do
-        matrix[$i,$j]=0
-    done
-done
+#for i in `seq 0 $rows` 
+#do
+#	echo $i
+#	for j in `seq 0 $cols` 
+#	do
+#        	matrix[$i,$j]=0
+#    	done
+#done
 
-echo $matrix
-exit
-
-
+echo "Finished building matrix"
+# parse actions
+actionnum=0
 while read line
 do
-	echo $line
+	(( actionnum++ ))
+	echo $actionnum
+	throughsplit1=`echo $line | awk -F"through" {'print $1'}`
+	throughsplit2=`echo $line | awk -F"through" {'print $2'}`
+	
+	[[ "$throughsplit1" =~ "toggle" ]] && action=t && taken=`echo $throughsplit1 | awk  {'print $2'}` 
+	[[ "$throughsplit1" =~ "turn off" ]] && action=d && taken=`echo $throughsplit1 | awk  {'print $3'}`
+	[[ "$throughsplit1" =~ "turn on" ]] && action=u && taken=`echo $throughsplit1 | awk  {'print $3'}`
+
+	min_x=`echo $taken | awk -F"," {'print $1'}`
+	min_y=`echo $taken | awk -F"," {'print $2'}`
+	max_x=`echo $throughsplit2 | awk -F"," {'print $1'}`
+	max_y=`echo $throughsplit2 | awk -F"," {'print $2'}`
+
+	echo "$actionnum is $action from $min_x,$min_y to $max_x,$max_y" >> log
+
+	for i in `seq $min_x $max_x`
+	do
+		for j in `seq $min_y $max_y`
+		do	
+			if [ -z "${matrix[$i,$j]}" ];
+				then 
+					matrix[$i,$j]=0
+			fi
+			[[ $action == 'u' ]] && matrix[$i,$j]=1
+			[[ $action == 'd' ]] && matrix[$i,$j]=0
+			[[ $action == 't' ]] && [[ $cur -eq 0 ]] && matrix[$i,$j]=1
+			[[ $action == 't' ]] && [[ $cur -eq 1 ]] && matrix[$i,$j]=0
+		done
+	done
 done < day6.csv
+
+echo "Finished taking actions"
+# sum
+sum=0
+for i in `seq 0 $rows`
+do
+        echo "Summing row $i"
+        for j in `seq 0 $cols`
+        do
+               [[ ${matrix[$i,$j]} -eq 1 ]] && (( sum++ ))
+        done
+done
+echo $sum
